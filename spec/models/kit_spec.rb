0bef627abaa74c1be20f31340f70866c00faea52
Mark Ertmer <mark.ertmer@gmail.com>
2022-05-14 10:55:20 -0600

    Add: kit & sample model specs

---
+++ spec/models/kit_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Kit, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_presence_of(:kick) }
+    it { should validate_presence_of(:hh_closed) }
+    it { should validate_presence_of(:hh_open) }
+    it { should validate_presence_of(:melody) }
+    it { should validate_presence_of(:texture) }
+    it { should validate_presence_of(:one_shot_1) }
+    it { should validate_presence_of(:one_shot_2) }
+    it { should validate_presence_of(:groove_url) }
+  end
+end
