25569c2423caf6f32da05a8b3619512573c2d7c5
Mark Ertmer <mark.ertmer@gmail.com>
2022-05-21 19:13:21 -0600

    Add: serializer methods

--- app/serializers/kit_serializer.rb
+++ app/serializers/kit_serializer.rb
@@ -5,4 +5,28 @@ class KitSerializer
       "kits": Kit.all.pluck(:name)
     }
   end
+
+  def self.kit_show(kit)
+    {
+      "name": kit.name,
+      "id": kit.id,
+      "elements": elements_hash(kit.elements),
+      "groove_url": kit.groove_url
+    }
+  end
+
+  def elements_hash(elements)
+    hash = {}
+    elements.each do |type, sample|
+      hash[type] = {
+        "name": sample.name,
+        "source_name": sample.source_name,
+        "sound_url": sample.sound_url,
+        "thumbnail_url": sample.thumbnail_url,
+        "description": sample.description,
+        "info_url": sample.info_url
+      }
+    end
+    return hash
+  end
 end
