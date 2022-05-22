10626fe28d02b1ef65f5526a16ea143ecba42ac7
Mark Ertmer <mark.ertmer@gmail.com>
2022-05-22 10:48:47 -0600

    Update: schema with new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -15,37 +15,16 @@ ActiveRecord::Schema.define(version: 2022_05_21_221153) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
   create_table "kits", force: :cascade do |t|
     t.string "name"
-    t.integer "kick"
-    t.integer "snare"
-    t.integer "hh_closed"
-    t.integer "hh_open"
-    t.integer "melody"
-    t.integer "texture"
-    t.integer "one_shot_1"
-    t.integer "one_shot_2"
+    t.string "kick"
+    t.string "snare"
+    t.string "hh_closed"
+    t.string "hh_open"
+    t.string "melody"
+    t.string "texture"
+    t.string "one_shot_1"
+    t.string "one_shot_2"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "groove_file"
@@ -62,5 +41,4 @@ ActiveRecord::Schema.define(version: 2022_05_21_221153) do
     t.string "thumbnail_file"
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
