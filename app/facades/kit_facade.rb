652c63270a4376badc6c7988362ad8e1193d209e
Mark Ertmer <mark.ertmer@gmail.com>
2022-05-21 19:12:35 -0600

    Add: facade methods

--- app/facades/kit_facade.rb
+++ app/facades/kit_facade.rb
@@ -3,4 +3,22 @@ class KitFacade
   def self.get_all_kits
     KitSerializer.all_kits
   end
+
+  def self.get_kit(kit)
+    samples = get_samples(kit)
+    the_kit = KitObject.new(kit, samples)
+    KitSerializer.kit_show(the_kit)
+  end
+
+  def self.get_samples(kit)
+    {
+      kick: SampleObject.new(kit[:kick]),
+      snare: SampleObject.new(kit[:snare]),
+      hh_closed: SampleObject.new(kit[:hh_closed]),
+      hh_open: SampleObject.new(kit[:hh_open]),
+      melody: SampleObject.new(kit[:melody]),
+      texture: SampleObject.new(kit[:texture]),
+      one_shot_1: SampleObject.new(kit[:one_shot_1]),
+      one_shot_2: SampleObject.new(kit[:one_shot_2])
+    }
 end
