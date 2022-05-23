01a68bf7bd36049e7f597b6bc87bc67c97349e04
Mark Ertmer <mark.ertmer@gmail.com>
2022-05-22 20:05:53 -0600

    Update: serializer hash key format

--- app/serializers/kit_serializer.rb
+++ app/serializers/kit_serializer.rb
@@ -2,29 +2,31 @@ class KitSerializer
 
   def self.all_kits
     {
-      "kits": Kit.all.pluck(:name)
+      "kits" => Kit.all.pluck(:name)
     }
   end
 
   def self.kit_show(kit)
     {
-      "name": kit.name,
-      "id": kit.id,
-      "elements": elements_hash(kit.elements),
-      "groove_url": kit.groove_url
+      "kit" => {
+        "name"=> kit.name,
+        "id"=> kit.id,
+        "elements"=> elements_hash(kit.elements),
+        "groove_url"=> kit.groove_url
+      }
     }
   end
 
-  def elements_hash(elements)
+  def self.elements_hash(elements)
     hash = {}
     elements.each do |type, sample|
       hash[type] = {
-        "name": sample.name,
-        "source_name": sample.source_name,
-        "sound_url": sample.sound_url,
-        "thumbnail_url": sample.thumbnail_url,
-        "description": sample.description,
-        "info_url": sample.info_url
+        "name" => sample.name,
+        "source_name" => sample.source_name,
+        "sound_url" => sample.sound_url,
+        "thumbnail_url" => sample.thumbnail_url,
+        "description" => sample.description,
+        "info_url" => sample.info_url
       }
     end
     return hash
