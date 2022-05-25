374575ba51ff1fd0564001c5b062a9baa933d87a
Austin Moore <austinchristianmoore@gmail.com>
2022-05-24 20:11:09 -0400

    Feature: Add sequence, BPM to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,26 @@ Kit.create(name: "Andromeda Strain",
   texture: "OS_Whistler Waves EMFISIS",
   one_shot_1: "OS_Deep Space Hubble",
   one_shot_2: "G_Passing Tempel 1 Stardust",
-  groove_file: "groove_stardust_passing_comet_temple_1.mp3"
+  groove_file: "groove_stardust_passing_comet_temple_1.mp3",
+  sequence: "[
+    ['C3', 'F#3'],
+    ['D#3', 'E3'],
+    ['D3'],
+    ['D#3'],
+    ['C#3', 'F#3'],
+    ['D3', 'E3', 'F#3'],
+    ['D#3'],
+    ['D3'],
+    ['C3'],
+    ['D#3', 'F3'],
+    ['D3', 'F3'],
+    ['F3'],
+    ['C#3', 'G3'],
+    ['D#3', 'F#3'],
+    ['D#3', 'F#3'],
+    ['C3', 'D3']
+  ]",
+  bpm: 180
 )
 
 Kit.create(name: "Magnetosphere",
@@ -22,7 +41,26 @@ Kit.create(name: "Magnetosphere",
   texture: "OS_Langmuir Waves Parker",
   one_shot_1: "OS_Whistler Mode 1 Parker",
   one_shot_2: "G_Shields Up Cassini",
-  groove_file: "groove_cassini_shields_up.mp3"
+  groove_file: "groove_cassini_shields_up.mp3",
+  sequence: '[
+    ["C3", "F#3"],
+    ["D3"],
+    ["D#3"],
+    ["E3"],
+    ["C#3"],
+    null,
+    ["D3"],
+    null,
+    ["C3", "F#3"],
+    null,
+    ["D3"],
+    null,
+    ["C#3", "F3"],
+    ["D3"],
+    ["D#3", "G3"],
+    ["C3"]
+    ]',
+  bpm: 260
 )
 
 Kit.create(name: "Apollo 11",
@@ -34,7 +72,26 @@ Kit.create(name: "Apollo 11",
   texture: "OS_One Small Step",
   one_shot_1: "OS_One Giant Leap",
   one_shot_2: "G_We Choose The Moon",
-  groove_file: "groove_jfk_wechoosethemoonspeech.mp3"
+  groove_file: "groove_jfk_wechoosethemoonspeech.mp3",
+  sequence: '[
+    ["C3", "D3"],
+    ["D3"],
+    ["D#3"],
+    ["D3"],
+    ["C3", "C#3", "D3"],
+    ["D3"],
+    ["D3"],
+    ["C3", "D3"],
+    ["C3", "D3"],
+    ["D3"],
+    ["D#3"],
+    ["D3"],
+    ["C#3", "C3", "D3"],
+    ["D3"],
+    ["D3"],
+    ["D3"],
+    ]',
+    bpm: 350
 )
 
 # Kicks
